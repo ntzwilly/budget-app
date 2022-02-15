807c8890bbb02c5cad15fab09af8702ce8b35cca
ntzwilly <ntzwilly@gmail.com>
2022-02-15 18:26:53 +0200

    Fix linter errors

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, :icon, presence: true
   belongs_to :user
   has_and_belongs_to_many :actions
+
+  def amount
+    actions.sum('amount')
+  end
 end
