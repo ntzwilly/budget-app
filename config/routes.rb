640e08d1b9ea20192656ae03109c06bb24452f2b
ntzwilly <ntzwilly@gmail.com>
2022-02-15 12:07:39 +0200

    Create splash, login and signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
   resources :categories
-  
-  root to: "categories#index"
+  resources :actions
+
+  root to: 'splash#index'
 end
