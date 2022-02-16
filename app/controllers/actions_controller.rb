4f5efebc6f4940cc9930bf56caa5bf2dcbce05f2
ntzwilly <ntzwilly@gmail.com>
2022-02-16 18:49:36 +0200

    Add style to the application

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,4 +1,6 @@
 class ActionsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     redirect_to categories_url
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :manage, Deal, author: user
+    can :manage, Action, author: user
     can :manage, Category, user: user
   end
 end
